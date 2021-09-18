8eceb9d68f28b1a1352992f0374b59f9607f9587
edupovaz <edu.povaz@gmail.com>
2021-09-18 19:11:24 -0300

    Ready Database

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const Farm = require('./modules/dbHelpers')
 const server = express()
 const PORT = 5000;
+const cors = require('cors')
 
 server.use(express.json())
+
+server.use(cors({origin: 'http://localhost:3000'}));
 
 
 server.get('/', (req,res) => {
     res.json({message: "RUNNING"})
+    
 })
 
 server.post('/api/farm', (req,res) =>{
@@ -116,3 +120,22 @@ server.patch('/api/gado/:id', (req,res) =>{
 server.listen(PORT, () => {
     console.log(`\n *** Server running on port ${PORT}`) 
 })
+
+server.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
